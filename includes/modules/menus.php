8e50029f0cfa094d44d1b44c33765b91b4c49c00
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-10 11:29:08 -0500

    Formatting for menus.php. #9

--- includes/modules/menus.php
+++ includes/modules/menus.php
@@ -4,7 +4,7 @@
  *
  * @package     EDD\ContentRestriction\Modules\Menus
  * @copyright   Copyright (c) 2013-2014, Pippin Williamson
- * @since       1.0.0
+ * @since       2.0
  */
 
 
@@ -12,8 +12,11 @@
 if( ! defined( 'ABSPATH' ) ) exit;
 
 function edd_cr_hide_menu_items( $items, $args ) {
+
     if( edd_get_option( 'edd_content_restriction_hide_menu_items', false ) ) {
+
         foreach( $items as $item_id => $item_data ) {
+
             $restricted = edd_cr_is_restricted( $item_data->object_id );
 
             $has_access = edd_cr_user_can_access( get_current_user_id(), $restricted, $item_data->object_id );
@@ -21,7 +24,9 @@ function edd_cr_hide_menu_items( $items, $args ) {
             if( $has_access['status'] == false ) {
                 unset( $items[$item_id] );
             }
+
         }
+
     }
 
     return $items;
